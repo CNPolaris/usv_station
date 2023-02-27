113ed41fa5406f1cdf3845b57182b4e9c9e35238
CNPolaris <cntianxin@outlook.com>
2023-02-27 14:58:11 +0800

    feat: 控制模块还行

--- layout/home.py
+++ layout/home.py
@@ -36,7 +36,12 @@ class ConnectProcessWidget(QWidget):
         self.setLayout(layout)
 
 class HomeWidget(QWidget):
-
+    ###############################
+    #主线程向子线程传值信号        #
+    ###############################
+    # 油门信号传递
+    left_signal = QtCore.Signal(int)
+    right_signal = QtCore.Signal(int)
     def __init__(self):
         super().__init__()
         self.home_form = Ui_HomeForm()
@@ -45,7 +50,9 @@ class HomeWidget(QWidget):
         self.home_form.MapWebView.page().settings().setAttribute(QWebEngineSettings.LocalContentCanAccessFileUrls, True)
         self.home_form.MapWebView.page().settings().setAttribute(QWebEngineSettings.LocalStorageEnabled, True)
         self.load_map()
+        ############################
         # 油门进度条
+        ############################
         layout = QHBoxLayout(self)
         self.left_staticPercentProgressBar = PercentProgressBar(self)
         self.left_staticPercentProgressBar.showFreeArea = True
@@ -56,24 +63,34 @@ class HomeWidget(QWidget):
         self.right_staticPercentProgressBar.ShowSmallCircle = True
         layout.addWidget(self.right_staticPercentProgressBar)
         self.home_form.process_layout.addLayout(layout)
-        # slider设置最大值最小值
+        # slider设置最大值最小值，启动时锁死，直到建立连接后解锁
         self.home_form.left_slider.setMinimum(0)
         self.home_form.left_slider.setMaximum(100)
         self.home_form.right_slider.setMaximum(0)
         self.home_form.right_slider.setMaximum(100)
+        self.home_form.left_slider.setDisabled(True)
+        self.home_form.right_slider.setDisabled(True)
+        # slider数据变化事件
         self.home_form.left_slider.valueChanged.connect(self.left_staticPercentProgressBar.setValue)
         self.home_form.right_slider.valueChanged.connect(self.right_staticPercentProgressBar.setValue)
+        self.home_form.left_slider.valueChanged.connect(self.left_signal.emit)
+        self.home_form.right_slider.valueChanged.connect(self.right_signal.emit)
+        ############################
         # 连接tcp服务器设置
+        ############################
         self.is_connect = True  # 状态变量 False时未连接 True时已经连接
         self.home_form.connect_tcp_btn.setIcon(QIcon(config.get_static_img_abs_path("start")))
         self.home_form.connect_tcp_btn.clicked.connect(self.on_connect_tcp_btn_clicked)
-        # 连接服务器进度条组件
-        self.connect_process_widget = ConnectProcessWidget()
-        # 子线程初始化
+        self.connect_process_widget = ConnectProcessWidget()  # 连接服务器进度条组件
+        ##################################
+        # 子线程
+        ##################################
         self.command_thread = CommandThread()
-        
         # 子线程向主线程通信
         self.command_thread.send_connect_flag_signal.connect(self.connect_process_bar)  # 绑定连接状态信号量
+        # 主线程向子线程通信
+        self.left_signal.connect(self.command_thread.send_left_accelerator_to_dtu)
+        self.right_signal.connect(self.command_thread.send_right_accelerator_to_dtu)
         
     def load_map(self):
         """
@@ -96,12 +113,14 @@ class HomeWidget(QWidget):
             self.home_form.connect_tcp_btn.setText("打开连接")
             self.home_form.connect_tcp_btn.setIcon(QIcon(config.get_static_img_abs_path("start")))
             self.is_connect = True
-            self.command_thread.quit()
+            self.command_thread.destroy()
     
     def connect_process_bar(self, flag):
         """连接服务器进度动画"""
-        if flag == 1:
+        if flag == 1:  # 连接服务器成功
             self.connect_process_widget.close()
+            self.home_form.left_slider.setDisabled(False)
+            self.home_form.right_slider.setDisabled(False)
             reply = QMessageBox.information(self, "服务连接", "连接成功", QMessageBox.Yes)
         else:
             reply = showMessage(self, "服务连接", "连接失败", QMessageBox.Yes)
--- threads/command_thread.py
+++ threads/command_thread.py
@@ -5,6 +5,10 @@
 # 地面站控制模块线程
 ################################################################################
 import socket
+import struct
+import json
+import subprocess
+
 from PySide6 import QtCore
 
 from utils.config import config
@@ -24,14 +28,64 @@ class CommandThread(QtCore.QThread):
       self.command_sock.connect((config.get_tcp_server_ip(), config.get_tcp_server_port()))
       self.command_sock.send(bytes(config.get_connect_key(), encoding='utf8'))
       self.send_connect_flag_signal.emit(1)  #连接成功
-      self.connect_flag = True
     except:
       print("连接服务器失败")
       self.send_connect_flag_signal.emit(0)  #连接失败
       
   def destroy(self):
     """关闭服务器连接"""
-    if self.connect_flag:
-      self.command_sock.close()
-      self.send_destroy_flag_signal.emit(1)  #关闭成功
-      self.connect_flag = False
+    self.send_msg_tcp_server(bytes("-1", encoding='utf-8'))
+    self.command_sock.close()
+    self.send_destroy_flag_signal.emit(1)  #关闭成功
+    self.quit()
+    
+  def send_left_accelerator_to_dtu(self, left):
+    """发送左油门信号到DTU"""
+    # self.command_sock.send(bytes(str(left), encoding='utf8'))
+    data = self.to_modbus_rtu(left)
+    # 制作报头
+    header_dic = {
+        'total_size': len(data)  # 总共的大小
+    }
+    header_json = json.dumps(header_dic) #字符串类型
+    header_bytes = header_json.encode('utf-8')  #转成bytes类型(但是长度是可变的)
+    # #先发报头的长度
+    self.command_sock.send(struct.pack('i',len(header_bytes))) #发送固定长度的报头
+    # #再发报头
+    self.command_sock.send(header_bytes)
+    # #最后发命令的结果
+    self.command_sock.send(data)
+    
+  def send_right_accelerator_to_dtu(self, right):
+    """发送右油门信号到DTU"""
+    data = self.to_modbus_rtu(right)
+    # 制作报头
+    header_dic = {
+        'total_size': len(data)  # 总共的大小
+    }
+    header_json = json.dumps(header_dic) #字符串类型
+    header_bytes = header_json.encode('utf-8')  #转成bytes类型(但是长度是可变的)
+    # #先发报头的长度
+    self.command_sock.send(struct.pack('i',len(header_bytes))) #发送固定长度的报头
+    # #再发报头
+    self.command_sock.send(header_bytes)
+    # #最后发命令的结果
+    self.command_sock.send(data)
+  
+  @staticmethod
+  def to_modbus_rtu(data):
+    return bytes(f"01 04 00 00 00 04 F1 {data}", encoding='utf-8')
+  
+  def send_msg_tcp_server(self, data):
+    # 制作报头
+    header_dic = {
+        'total_size': len(data)  # 总共的大小
+    }
+    header_json = json.dumps(header_dic) #字符串类型
+    header_bytes = header_json.encode('utf-8')  #转成bytes类型(但是长度是可变的)
+    # #先发报头的长度
+    self.command_sock.send(struct.pack('i',len(header_bytes))) #发送固定长度的报头
+    # #再发报头
+    self.command_sock.send(header_bytes)
+    # #最后发命令的结果
+    self.command_sock.send(data)
